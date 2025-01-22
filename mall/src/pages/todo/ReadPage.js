1142ccf3de3e6b9d622f0241669b810f67a5126c
Jeong Soo <naglsa@naver.com>
2025-01-22 15:37:18 +0900

    25-01-22  front 커밋 확인용

--- mall/src/pages/todo/ReadPage.js
+++ mall/src/pages/todo/ReadPage.js
@@ -1,44 +1,46 @@
-import React from 'react';
-import { createSearchParams, useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import React from "react";
+import {
+  createSearchParams,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 
 function ReadPage(props) {
+  const navigate = useNavigate();
+  const { tno } = useParams();
 
-    const navigate = useNavigate();
-    const { tno } = useParams();
+  const [queryParams] = useSearchParams();
 
-    const [queryParams]= useSearchParams();
+  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
+  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
 
-    const page = queryParams.get('page')? parseInt(queryParams.get('page')): 1;
-    const size = queryParams.get('size')? parseInt(queryParams.get('size')): 10;
+  // 쿼리스트링을 만들어주는 함수, 페이지 이동할때 queryStr이 따라다님
+  // 수정확인
+  const queryStr = createSearchParams({ page: page, size: size }).toString();
 
-    const queryStr = createSearchParams({page:page, size:size}).toString();
+  const moveToModify = (tno) => {
+    navigate({
+      pathname: `/todo/modify/${tno}`,
+      search: queryStr,
+    });
+  };
 
-    const moveToModify = (tno) => {
-        navigate({
-            pathname: `/todo/modify/${tno}`,
-            search: queryStr
-        })        
-    };
+  const moveToList = () => {
+    navigate({
+      pathname: "/todo/list",
+      search: queryStr,
+    });
+  };
 
-    const moveToList = () => {
-        navigate({
-            pathname: '/todo/list',
-            search: queryStr
-        })
-    };
-
-
-    return (
-        <div className={'text-3xl'}>
-            Todo Read Page {tno}
-
-            {/* 내가 props로 써야할것과 라우터로 써야할것을 구분하는게 좋음. */}
-            
-            <button onClick={() => moveToModify(tno)}> Test Modify </button>
-            <button onClick={moveToList}> Test List </button>
-
-        </div>
-    );
+  return (
+    <div className={"text-3xl"}>
+      Todo Read Page {tno}
+      {/* 내가 props로 써야할것과 라우터로 써야할것을 구분하는게 좋음. */}
+      <button onClick={() => moveToModify(tno)}> Test Modify </button>
+      <button onClick={moveToList}> Test List </button>
+    </div>
+  );
 }
 
 export default ReadPage;
